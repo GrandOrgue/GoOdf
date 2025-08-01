592e6ef59dc2680c9ee534371ba78ef2c7b80827
Lars Palo <lars.birger.palo@gmail.com>
2025-08-01 22:32:20 +0200

    Attempt to fix empty converted icon logotypes.

--- scripts/prepare-ubuntu.sh
+++ scripts/prepare-ubuntu.sh
@@ -5,5 +5,5 @@ set -e
 sudo apt update
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential cmake \
-  libwxgtk3.0-gtk3-dev imagemagick
+  libwxgtk3.0-gtk3-dev gsfonts imagemagick
 
